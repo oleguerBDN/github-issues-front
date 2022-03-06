37c763fd63a2befd6b98e0f22dca452fc585d608
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2022-03-06 13:34:39 +0100

    Added routing between pages in App.js, * go to IssuesPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import IssueDetailPage from "./pages/IssueDetailPage";
 import IssuesPage from "./pages/IssuesPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <IssuesPage />
+        <Routes>
+          <Route path="/issues" element={<IssuesPage />} />
+          <Route path="/issue/:id" element={<IssueDetailPage />} />
+          <Route path="/*" element={<Navigate to="/issues" />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
