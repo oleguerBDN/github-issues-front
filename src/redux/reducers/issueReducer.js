2f14f3e33397f418c08503c3beef277e33ec4910
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2022-03-06 11:46:08 +0100

    Added redux issueReducer

---
+++ src/redux/reducers/issueReducer.js
@@ -0,0 +1,27 @@
+import actionTypes from "../actions/actionTypes";
+
+const issueReducer = (issue = {}, action) => {
+  let newIssue;
+  switch (action.type) {
+    case actionTypes.loadIssue:
+      newIssue = { ...action.issue };
+      break;
+    case actionTypes.errorIssue:
+      newIssue = {
+        ...issue,
+        isError: true,
+        errorMessage: action.errorMessage,
+      };
+      break;
+    case actionTypes.cleanIssue:
+      newIssue = { isError: false };
+      break;
+    default:
+      newIssue = { ...issue, isError: false };
+      break;
+  }
+
+  return newIssue;
+};
+
+export default issueReducer;
