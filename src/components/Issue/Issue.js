a064dfb018de518a4b5ad26cd909803ed21492be
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2022-03-06 11:00:21 +0100

    Adedd Issue component

---
+++ src/components/Issue/Issue.js
@@ -0,0 +1,13 @@
+const Issue = ({ title, body, state, author, authorUrl }) => (
+  <div className="issue">
+    <h2 className="issue__title">{title}</h2>
+    <span className="issue__state">{state}</span>
+    <a className="issue__author" href={authorUrl}>
+      {author}
+    </a>
+    <div className="markdown-body">
+      <div dangerouslySetInnerHTML={{ __html: body }} />
+    </div>
+  </div>
+);
+export default Issue;
