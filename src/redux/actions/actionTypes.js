dd6bcd9fb2361886653eed7e957f1aba6c9e6b0b
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2022-03-06 11:43:38 +0100

    Added redux actionTypes

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,10 @@
+const actionTypes = {
+  loadIssues: "LOAD_ISSUES",
+  loadIssue: "LOAD_ISSUE",
+  loadComments: "LOAD_COMMENTS",
+  errorIssues: "ERROR_ISSUES",
+  errorIssue: "ERROR_ISSUE",
+  cleanIssue: "CLEAN_ISSUE",
+};
+
+export default actionTypes;
